da58f624c51e8efa131c22362b9287bc1c150e0f
尹帅 <1317972280@qq.com>
2024-08-26 15:47:21 +0800

    udpate

--- app/src/main/java/com/ooimi/v2ray/MainActivity.kt
+++ app/src/main/java/com/ooimi/v2ray/MainActivity.kt
@@ -3,15 +3,19 @@ package com.ooimi.v2ray
 import android.content.Intent
 import android.net.VpnService
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.v2ray.ang.V2rayCallback
 import com.v2ray.ang.V2raySDK
 import com.v2ray.ang.bean.Socks5Config
 import com.v2ray.ang.ui.V2RayMainActivity
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private val requestVpnPermission =
@@ -21,13 +25,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
         }
 
-    private val socks5Config = Socks5Config().apply {
-        host = "59.63.213.95"
-        port = 21080
-        userName = "wadada"
-        password = "adadadad"
-        proxyApp = arrayListOf("com.tencent.mobileqq")
-    }
+    private val socks5Config = Socks5Config()
 
 
     private val callback = object : V2rayCallback {
@@ -40,6 +38,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -48,9 +47,33 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         findViewById<Button>(R.id.uiBtn).setOnClickListener(this)
     }
 
+
+    private fun getInputText(id: Int): String {
+        return try {
+            findViewById<EditText>(id).text.toString() ?: ""
+        } catch (e: Exception) {
+            e.printStackTrace()
+            ""
+        }
+    }
+
     override fun onClick(view: View?) {
         when (view?.id) {
             R.id.startBtn -> {
+                if (TextUtils.isEmpty(getInputText(R.id.host))) {
+                    Toast.makeText(this, "host不能为空", Toast.LENGTH_SHORT).show()
+                    return
+                }
+                if (TextUtils.isEmpty(getInputText(R.id.port))) {
+                    Toast.makeText(this, "port不能为空", Toast.LENGTH_SHORT).show()
+                    return
+                }
+                val proxyApp = getInputText(R.id.proxyApp).split(",") ?: arrayListOf()
+                socks5Config.host = getInputText(R.id.host)
+                socks5Config.port = getInputText(R.id.port)?.toInt() ?: 0
+                socks5Config.userName = getInputText(R.id.userName)
+                socks5Config.password = getInputText(R.id.password)
+                socks5Config.proxyApp = proxyApp
                 val intent = VpnService.prepare(this)
                 if (intent == null) {
                     V2raySDK.startSocks5Proxy(this, socks5Config, callback)
